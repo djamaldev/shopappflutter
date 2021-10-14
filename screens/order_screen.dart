4301dc7e8202528e524a01905aa25fe4f03fa16d
djamaldev <34842155+djamaldev@users.noreply.github.com>
2021-10-14 11:12:45 +0100

    update
    add order screen

--- screens/order_screen.dart
+++ screens/order_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/orders.dart';
 import 'package:shopapp/widgets/app_drawer.dart';
+import 'package:shopapp/widgets/order_item.dart';
 
 class OrederScreen extends StatelessWidget {
   static const routName = '/order';
@@ -7,13 +10,32 @@ class OrederScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Orders'),
-      ),
-      body: Center(
-        child: Text('Oredrs'),
-      ),
-      drawer: AppDrawer(),
-    );
+        appBar: AppBar(
+          title: Text('Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrder(),
+          builder: (ctx, snapShot) {
+            if (snapShot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else {
+              if (snapShot.error != null) {
+                return Center(
+                  child: Text('An error occurd'),
+                );
+              } else {
+                return Consumer<Orders>(
+                  builder: (cx, oredrData, child) => ListView.builder(
+                    itemCount: oredrData.order.length,
+                    itemBuilder: (_, index) =>
+                        OrderItem(oredrData.order[index]),
+                  ),
+                );
+              }
+            }
+          },
+        ));
   }
 }
